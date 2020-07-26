bf110fea392182cf93a0ca722f6e94d7dec0b50e
Jakub Noga <noga.jakub@gmail.com>
2020-07-26 22:01:27 +0200

    basic benchmarking

--- main.go
+++ main.go
@@ -1,15 +1,89 @@
 package main
 
 import (
+	"time"
+	"image/jpeg"
+	"image/color"
+	"bufio"
 	"fmt"
-	"kdtree/kdtree"
+	"image"
+	"log"
+	"os"
+	"path/filepath"
+	"processing"
 )
+import _ "image/jpeg"
+import _ "image/png"
+import _ "image/gif"
+
+var _ = fmt.Print
+var _ = color.Alpha16Model
 
 func main() {
-	tree := kdtree.Create([][]float64{{2,3}, {5,4}, {9,6}, {4,7}, {8,1}, {7,2}}, 0)
+	path, _ := filepath.Abs("./processing/assets/przedpole.jpg")
+	file, e := os.Open(path)
+	if e != nil {
+		log.Fatalf("%v", e)
+	}
+	defer file.Close()
 
-	best := tree.NearestNeighbour([]float64{10,10})
+	paletteName := os.Args[1]
 
-	fmt.Println(tree.ToJson())
-	fmt.Println(best.Point)
+	palettePath, _ := filepath.Abs("./processing/assets/" + paletteName)
+	palettefile, e := os.Open(palettePath)
+	if e != nil {
+		log.Fatalf("%v", e)
+	}
+	defer palettefile.Close()
+
+	img, _, e := image.Decode(bufio.NewReader(file))
+	if e != nil {
+		log.Fatalf("%v", e)
+	}
+
+	hexReader := new(processing.HexReader)
+	palette, _ := hexReader.Read(palettefile) 
+
+	// log.Printf("%v", palette)
+
+	convertAndSave(img, palette, "kd_output.jpg", kdtree)
+	convertAndSave(img, palette, "naive_output.jpg", naive)
+}
+
+func convertAndSave(input image.Image, palette color.Palette, outputFileName string, converter func (input image.Image, palette color.Palette) image.Image ) {
+	img := converter(input, palette)
+
+	path, _ := filepath.Abs("./processing/assets/" + outputFileName)
+	file, e := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0755)
+	if e != nil {
+		log.Fatalf("%v", e)
+	}
+	defer file.Close()
+
+	e = jpeg.Encode(bufio.NewWriter(file), img, nil)
+	if e != nil {
+		log.Fatalf("%v", e)
+	}
+}
+
+func kdtree(input image.Image, palette color.Palette) image.Image {
+	defer duration(track("KdTree coversion took"))
+	p := processing.NewKdTreeProcessor(palette)
+	
+	return processing.ConvertImage(input, p)
+}
+
+func naive(input image.Image, palette color.Palette) image.Image {
+	defer duration(track("Naive coversion took"))
+	p := processing.NewNaiveProcessor(palette)
+
+	return processing.ConvertImage(input, p)
+}
+
+func track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func duration(msg string, start time.Time) {
+	log.Printf("%v: %v\n", msg, time.Since(start))
 }
